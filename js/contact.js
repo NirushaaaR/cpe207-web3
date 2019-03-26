2f8f0470357f50bab8707c37574d95a6ac0e163f
nirushsrijaimoon <nirushsrijaimoon@gmail.com>
2019-03-26 20:00:34 +0700

    finish

--- js/contact.js
+++ js/contact.js
@@ -1,7 +1,18 @@
 const myform = document.querySelector("#main-form");
 const ulContactList = document.querySelector("#show-contact");
-const gender = "gender";
 const formsInformation = document.querySelectorAll(".form-group.row");
+const gender = "gender";
+const dataKey = "GetJson!!";
+
+const oldData = localStorage.getItem(dataKey);
+
+let contactList = oldData === null ? [] : JSON.parse(oldData);
+
+//retrieve old data
+contactList.forEach( (elem, index) =>{
+    updateContactList(elem, index);
+});
+
 
 // add validiation
 formsInformation.forEach(function(form) {
@@ -15,32 +26,75 @@ myform.addEventListener("submit", addContactList);
 function addContactList(e){
     // prevent reload
     e.preventDefault();
-    // storing data
-    let li = document.createElement("li");
+    // storage data
+    let data = {};
 
     formsInformation.forEach(function(form){
         const label = form.querySelector("label").getAttribute("for");
-        let getValue = "";
         if(label === "gender"){
             // check radio button
             form.querySelectorAll("input").forEach(function(check){
                 if(check.checked){
-                    getValue = check.value;
+                    data[label] = check.value;
                 }
-            })
-
+            });
         }else{
             const value = form.querySelector(".input-form").value;
-            getValue = value;
+            data[label] = value;
         }
+    });
 
+    contactList.push(data);
+    updateContactList(data, contactList.length-1);
+    // store to local storage
+    var jsonString = JSON.stringify(contactList);
+    localStorage.setItem(dataKey, jsonString);
+}
+
+function updateContactList(data,index){
+    // li element to contain contact data
+    const li = document.createElement("li");
+    li.setAttribute("id", index)
+    li.classList.add("list-group-item", "text-dark", "d-flex", "justify-content-between");
+
+    //contain data
+    const divP = document.createElement("div");
+    divP.classList.add("flex-fill");
+    // loop through object
+    Object.entries(data).forEach( entry => {
         const p = document.createElement("p");
-        p.textContent = `${label} : ${getValue}`;
-        li.appendChild(p);
+        p.classList.add("p-2","bg-light","border","border-dark","border-round");
+        p.textContent = `${entry[0]} : ${entry[1]}`;
+        divP.appendChild(p);
+    });
+    li.appendChild(divP);
+
+    // button for delete
+    const btn = document.createElement("button");
+    btn.classList.add("btn","btn-danger", "btn-sm", "delete", "align-self-start","ml-3");
+    btn.textContent = "X";
+    li.appendChild(btn);
+    btn.addEventListener("click", (e)=>{
+        const temp_li = e.target.parentNode;
+        const ul = temp_li.parentNode;
+        const check = ul.children;
+        // find index
+        for(let i =0; i<ul.childElementCount; i++){
+            if(check[i] === temp_li){
+                removeContactData(i-1);
+            }
+        }
+        temp_li.parentNode.removeChild(temp_li);
     });
 
     ulContactList.appendChild(li);
 }
 
-
-//was-validated
+function removeContactData(id){
+    contactList = contactList.filter((_,index)=>{
+        return index != id;
+    });
+    // store to local storage
+    var jsonString = JSON.stringify(contactList);
+    localStorage.setItem(dataKey, jsonString);
+}
